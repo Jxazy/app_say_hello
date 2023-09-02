72c90eb361de38a5ff965aa154e0897ee0823af9
Fajar Octhaviano <fajarduaa@gmail.com>
2023-09-02 13:26:53 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Jxazy/app_say_hello
+
+go 1.21.0
+
+require github.com/Jxazy/example_module/v2 v2.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Jxazy/example_module/v2"
+)
+
+func main() {
+	fmt.Println(example_module.SayHello("Fajar"))
+}
